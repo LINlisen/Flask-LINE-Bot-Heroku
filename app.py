be97df03b28458d6960005d502c4534bbb3bafda
LINlisen <z1x2c3v441@gmail.com>
2022-03-17 21:42:55 +0800

    test

--- app.py
+++ app.py
@@ -43,32 +43,32 @@ def callback():
         return "OK"
     
 
-def Starting_Qusetion(q_num):
-    buttons_template_message = TemplateSendMessage(
-                    alt_text='Buttons template',
-                    template=ButtonsTemplate(
-                        thumbnail_image_url='https://example.com/image.jpg',
-                        title='問題'+String(q_num+1),
-                        text=q[q_num]['text'],
-                        actions=[
-                            PostbackAction(
-                                label=q[q_num]['answer'][0]['label'],
-                                display_text=q[q_num]['answer'][0]['label'],
-                                data=q[q_num]['answer'][0]['data']
-                            ),
-                            PostbackAction(
-                                label=q[q_num]['answer'][1]['label'],
-                                display_text=q[q_num]['answer'][1]['label'],
-                                data=q[q_num]['answer'][1]['data']
-                            ),
-                            PostbackAction(
-                                label=q[q_num]['answer'][1]['label'],
-                                display_text=q[q_num]['answer'][1]['label'],
-                                data=q[q_num]['answer'][1]['data']
-                            ),
-                        ]
-                    )
-                )    
+# def Starting_Qusetion(q_num):
+#     buttons_template_message = TemplateSendMessage(
+#                     alt_text='Buttons template',
+#                     template=ButtonsTemplate(
+#                         thumbnail_image_url='https://example.com/image.jpg',
+#                         title='問題'+String(q_num+1),
+#                         text=q[q_num]['text'],
+#                         actions=[
+#                             PostbackAction(
+#                                 label=q[q_num]['answer'][0]['label'],
+#                                 display_text=q[q_num]['answer'][0]['label'],
+#                                 data=q[q_num]['answer'][0]['data']
+#                             ),
+#                             PostbackAction(
+#                                 label=q[q_num]['answer'][1]['label'],
+#                                 display_text=q[q_num]['answer'][1]['label'],
+#                                 data=q[q_num]['answer'][1]['data']
+#                             ),
+#                             PostbackAction(
+#                                 label=q[q_num]['answer'][1]['label'],
+#                                 display_text=q[q_num]['answer'][1]['label'],
+#                                 data=q[q_num]['answer'][1]['data']
+#                             ),
+#                         ]
+#                     )
+#                 )    
                 
 
 
@@ -77,27 +77,28 @@ def Starting_Qusetion(q_num):
 def handle_message(event):
     get_message = event.message.text
     if get_message == '開始問答':
-        Starting_Qusetion(count)
+        # 
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text='獲得獎勵！'))
 
-@handler.add(PostbackAction, message=PostbackAction)
-def handle_postback(event):
-    get_postback = event.postback.data
-    if(get_postback == '答對'):
-        try:
-            line_bot_api.reply_message(event.reply_token,TextSendMessage(text='恭喜答對！'))
-            count+=1
-            Starting_Qusetion(count)
-        except:
-            line_bot_api.reply_message(event.reply_token,TextSendMessage(text='發生錯誤！'))
+# @handler.add(PostbackAction, message=PostbackAction)
+# def handle_postback(event):
+#     get_postback = event.postback.data
+#     if(get_postback == '答對'):
+#         try:
+#             line_bot_api.reply_message(event.reply_token,TextSendMessage(text='恭喜答對！'))
+#             count+=1
+#             Starting_Qusetion(count)
+#         except:
+#             line_bot_api.reply_message(event.reply_token,TextSendMessage(text='發生錯誤！'))
 
-    else:
-        try:
-            line_bot_api.reply_message(event.reply_token,TextSendMessage(text='答錯！'))
-            Starting_Qusetion(count)
-        except:
-            line_bot_api.reply_message(event.reply_token,TextSendMessage(text='發生錯誤！'))
-    if(count==2):
-        try:
-            line_bot_api.reply_message(event.reply_token,TextSendMessage(text='獲得獎勵！'))
-        except:
-            line_bot_api.reply_message(event.reply_token,TextSendMessage(text='發生錯誤！'))
+#     else:
+#         try:
+#             line_bot_api.reply_message(event.reply_token,TextSendMessage(text='答錯！'))
+#             Starting_Qusetion(count)
+#         except:
+#             line_bot_api.reply_message(event.reply_token,TextSendMessage(text='發生錯誤！'))
+#     if(count==2):
+#         try:
+#             line_bot_api.reply_message(event.reply_token,TextSendMessage(text='獲得獎勵！'))
+#         except:
+#             line_bot_api.reply_message(event.reply_token,TextSendMessage(text='發生錯誤！'))
