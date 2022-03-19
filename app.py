7dbd5a1d77a0ff3727ad34ec168e6df29f3510b7
LINlisen <z1x2c3v441@gmail.com>
2022-03-19 23:30:31 +0800

    change get userid func

--- app.py
+++ app.py
@@ -14,25 +14,12 @@ with open("/app/question.json") as f:
 with open("/app/data.json") as g:
     s = json.load(g)
 app = Flask(__name__)
-profile = ""
 line_bot_api = LineBotApi(os.environ.get("CHANNEL_ACCESS_TOKEN"))
 handler = WebhookHandler(os.environ.get("CHANNEL_SECRET"))
 
-try:
-    profile = line_bot_api.get_profile('<user_id>')
-    print(profile)
-except LineBotApiError as e:
-    print(e)
-if(profile == None):
-    print("Did not get username")
-if(profile not in s.keys()):
-    new_user={"{profile}":0}
-    s.update(new_user)
-    with open("/app/data.json",'w',encoding='utf-8') as h:
-        json.dump(s, h,ensure_ascii=False)
+
 @app.route("/", methods=["GET", "POST"])
 def callback():
-
     if request.method == "GET":
         return "Hello Heroku"
     if request.method == "POST":
@@ -79,22 +66,24 @@ def Starting_Qusetion(q_num):
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    try:
-        profile = line_bot_api.get_profile('<user_id>')
-        print(profile)
-    except LineBotApiError as e:
-        print(e)
+    user_id = event.source.user_id
+    print("user_id =", user_id)
+    if(user_id not in s.keys()):
+        new_user={"{user_id}":0}
+        s.update(new_user)
+        with open("/app/data.json",'w',encoding='utf-8') as h:
+            json.dump(s, h,ensure_ascii=False)
     get_message = event.message.text
     if get_message == '開始問答':
         with open("/app/data.json") as g:
                 s = json.load(g)
-        if(s[profile]==2):
+        if(s[user_id]==2):
             try:
                 line_bot_api.reply_message(event.reply_token,TextSendMessage('已獲得獎勵！')) 
             except:
                 line_bot_api.reply_message(event.reply_token,TextSendMessage(text='發生錯誤！'))    
         else:
-            templete_button=Starting_Qusetion(s[profile])
+            templete_button=Starting_Qusetion(s[user_id])
             try:
                 line_bot_api.reply_message(event.reply_token,templete_button) 
             except:
@@ -102,28 +91,25 @@ def handle_message(event):
         
 @handler.add(PostbackEvent)
 def handle_postback(event):
-    try:
-        profile = line_bot_api.get_profile('<user_id>')
-        print(profile)
-    except LineBotApiError as e:
-        print(e)
+    user_id = event.source.user_id
+    print("user_id =", user_id)
     get_postback = event.postback.data
     print(get_postback)
     with open("/app/data.json") as g:
                 s = json.load(g)
-    if(s[profile]==2):
+    if(s[user_id]==2):
         try:
             line_bot_api.reply_message(event.reply_token,TextSendMessage('獲得獎勵！'))
         except:
             line_bot_api.reply_message(event.reply_token,TextSendMessage('發生錯誤！'))
-    if(get_postback == '答對' and s[profile]!=2):
+    if(get_postback == '答對' and s[user_id]!=2):
         try:
-            dict={profile:s[profile]+1}
+            dict={user_id:s[user_id]+1}
             with open("/app/data.json",'w',encoding='utf-8') as h:
                 json.dump(dict, h,ensure_ascii=False)
             with open("/app/data.json") as g:
                 s = json.load(g)
-            next_templete_button=Starting_Qusetion(s[profile])
+            next_templete_button=Starting_Qusetion(s[user_id])
             print(next_templete_button)
             line_bot_api.reply_message(event.reply_token,next_templete_button) 
         except:
